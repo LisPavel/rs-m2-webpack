9e0258e064de015c2cc65e3513020465f56a39fc
Pavel Lisitsyn <43790794+LisPavel@users.noreply.github.com>
2024-01-06 14:43:32 +0300

    chore: add dev server

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -4,4 +4,9 @@ const commonCfg = require("./webpack.config.common");
 module.exports = merge(commonCfg, {
   mode: "development",
   devtool: "inline-source-map",
+  devServer: {
+    port: 3000,
+    hot: true,
+    open: true,
+  },
 });
