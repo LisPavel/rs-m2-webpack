378742f9db262be71ad680dc3a77fb9ad2b874ec
Pavel Lisitsyn <43790794+LisPavel@users.noreply.github.com>
2024-01-06 14:11:18 +0300

    chore: add css to build

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,7 +1,14 @@
 const { merge } = require("webpack-merge");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+
 const commonCfg = require("./webpack.config.common");
 
 module.exports = merge(commonCfg, {
   mode: "production",
   devtool: "source-map",
+  optimization: {
+    minimize: true,
+    minimizer: [new CssMinimizerPlugin(), new TerserPlugin()],
+  },
 });
